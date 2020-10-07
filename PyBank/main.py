f7a470945058a36d52fa164b99e6ed298c33dbb4
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2020-10-07 00:00:32 -0400

    Added the output to the terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,20 @@ greatloss = 0
 darkday = ""
 
 
+def generateoutput(months, total, average, goodday, maxprofit, badday, maxloss):
+    # This clears the terminal so that our analysis will not be crowded out
+    os.system('clear')
+    print('Financial Analysis')
+    print('--------------------')
+    print('Total Months: ' + str(months))
+    print('Total: $' + str(total))
+    print('Average Change: $' + str(average))
+    print('Greatest Increase in Profits: ' + goodday + " ($" + str(maxprofit) + ")")
+    print('Greatest Decrease in Profits: ' + badday + " ($" + str(maxloss) + ")")
+
+
+
+
 
 with open(budgetdata, 'r') as budgetfile:
     csvfile = csv.reader(budgetfile, delimiter = ',')
@@ -35,15 +49,14 @@ with open(budgetdata, 'r') as budgetfile:
             # Special case for the first loop
             # Initial change is set to 0 to avoid skewing average change on the high side
             # Store the previous line's profit/loss for calculation of change in next iteration
+            currentpl = float(i[1])
             changes.append(0)
             previouspl = float(i[1])
-            currentpl = 0
 
         else:
             # Grab the current profit loss then calculate average and move current to previous
             currentpl = float(i[1])
             changes.append(currentpl-previouspl)
-            print(currentpl-previouspl)
             previouspl = currentpl
 
         # This if statement evaluates whether the current change is of greater magnitude than the current "winners"
@@ -63,6 +76,5 @@ with open(budgetdata, 'r') as budgetfile:
     # We only count the rows where we get a real change
     # Also round because we don't really need those extra 11 decimal places
     avgpl = round(totalpl / (count-1), 2)
-    print(avgpl)
-    print("Greatest Gain: " + brightday + ": " + str(greatgain))
-    print("Greatest Loss: " + darkday + ": " + str(greatloss))
+
+    generateoutput(count, int(profit), avgpl, brightday, int(greatgain), darkday, int(greatloss))
