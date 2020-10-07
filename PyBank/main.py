72629c8163956bbe5c7e04c33263cca0509e1ea8
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2020-10-06 23:14:46 -0400

    Add in count for number of months and profit summing

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,20 @@ import csv
 
 
 budgetdata = os.path.join('Resources', 'budget_data.csv')
+count = 0
+profit = 0
 
-with open(budgetdata,'r') as budgetfile:
+
+
+with open(budgetdata, 'r') as budgetfile:
     csvfile = csv.reader(budgetfile, delimiter = ',')
 
+    # Remove the headers and store them just in case
+    header = next(csvfile)
+
     for i in csvfile:
-        print(i)
+        # Count will hold on to the number of lines that we find in the file
+        count += 1
+        profit = profit + float(i[1])
+
+    print(profit)
